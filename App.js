7f26603a3d1fb74403ff0a1e6a281136a8ede516
Megyeri Márk Máté <megyko134@gmail.com>
2023-02-27 10:49:45 +0100

    Kinézet oké

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
+  const [radius,setRadius] =useState('');
+  const [height,setHeight] =useState('');
+  const [volume,setvolume] =useState('');
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Kúp térfogatának kiszámítása</Text>
+      <Text>Sugár</Text>
       <StatusBar style="auto" />
+    <TextInput
+    style={styles.inputField}
+    onChangeText= {(radius) => setRadius(radius)}></TextInput>
+      <Text>Magasság</Text>
+      <StatusBar style="auto" />
+    <TextInput
+    style={styles.inputField}
+    onChangeText= {(height) => setHeight(height)}></TextInput>
+      <Text>Eredény</Text>
+      <StatusBar style="auto" />
+    <TextInput
+    style={styles.inputField}
+    onChangeText= {(volume) => setvolume(volume)}></TextInput>
+
+<TouchableHighlight
+style={styles.button}
+
+><Text>Számít</Text></TouchableHighlight>
+
+
     </View>
   );
 }
 
+
+
+
+
+
+
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a570c4',
     alignItems: 'center',
     justifyContent: 'center',
+
+   
   },
+  inputField:{
+    backgroundColor:"#c5f3fd",
+    borderRadius:'25px',
+    width :'90%',
+  },
+  button: {
+    backgroundColor: '#f3eadb',
+    borderRadius:'25px',
+    padding:"17px",
+    margin:"15px",
+  }
 });
